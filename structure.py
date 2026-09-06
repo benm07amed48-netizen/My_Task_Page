4b31af58a594216578b962b8407a63666f4db9d1
Mohamed <mohamed.benkhira.dev@gmail.com>
2026-09-06 13:58:09 +0100

    structure file completed

--- structure.py
+++ structure.py
@@ -9,8 +9,15 @@ tool.logs()
 
 instr={
 
-    "l":["todo","done"],
-    "e":["info","stat"],
+    "l":{
+        "all":tool.l_all,
+        "todo":tool.l_todo,
+        "done":tool.l_done
+        },
+    "e":{
+        "info":"",
+        "stat":""
+        },
     "a":"",
     "d":"",
     "i":""
@@ -19,9 +26,19 @@ instr={
 if len (sys.argv)>1:
     command=sys.argv[1:]
 else:
-    command=input(": _ ").strip().strip("_").strip(".").split(" ")
-    print(type(command))
-tool.act(command)
-
+    command=input(":_ ").strip().strip("_").strip(".").split()
+if len(command)>3:
+    print("you input more than 3 inputs 'only 3 or less required'")
 else:
-    print ("You didnn't input a validate command or misspelled it check the instruction agian") 
+    if command[0].lower() in instr:
+        if len(command)==2:
+            if command[1] in instr[command[0]]:
+                instr[command[0]][command[1]]()
+            else :
+                instr[command[0]](command[1])
+        elif command[1] in instr[command[0]]:
+            instr[command[0]][command[1]](command[2])
+        else:
+            print("You didn't inout a valid command 'the second is wrong' ")
+    else: 
+        print("You didn't inout a valid command 'the first is wrong' ")
